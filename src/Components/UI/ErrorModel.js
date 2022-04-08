2e705ad3812537517c0b6e7c298d074469b418b7
Siva <sivathebatman@gmail.com>
2022-04-08 14:54:31 +0530

    error model added

---
+++ src/Components/UI/ErrorModel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+
+const ErrorModel = (props) => {
+
+    return (
+        <Card className="">
+
+        </Card>
+    )
+}
+
+export default ErrorModel;
