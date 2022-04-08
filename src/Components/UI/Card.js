238db60446f2c314f7b3a9ed21a1f7614f4e601a
Siva <sivathebatman@gmail.com>
2022-04-08 12:09:22 +0530

    classes added to the custom HTML component

--- src/Components/UI/Card.js
+++ src/Components/UI/Card.js
@@ -4,7 +4,7 @@ import styles from "./Card.module.css";
 const Card = (props) => {
 
     return (
-        <div className={styles.card}>
+        <div className={`${styles.card} ${props.className}`}>
             {props.children}
         </div>
     )
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
 
-const AddUser = () => {
+const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
     }
 
     return (
-        <Card>
-            <form onSubmit={addUserHandler}>
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler} >
                 <label htmlFor="userName">User Name</label>
                 <input type="text" id="username" />
                 <label htmlFor="age">Age</label>
